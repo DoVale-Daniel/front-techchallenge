e2d8ec31a583e682196396eb7bdc37e110149486
Eric do Carmo Barbosa <ericcarmobarbosa@gmail.com>
2018-07-21 19:22:41 -0300

    Telas de TransportUnit e Invoice

--- src/views/PageRegisterInvoice.vue
+++ src/views/PageRegisterInvoice.vue
@@ -1,37 +1,94 @@
 <template>
   <BcPageBase 
     title="Cadastrar Notas Fiscais"
-    class="page page-invoice">
+    class="page page-home">
 
     <template slot="content">
       <form class="form">
         <b-field label="Dados da NF:">
-          <b-input type="textarea"></b-input>
+          <b-input 
+            type="textarea"
+            v-model="invoiceJSON" />
         </b-field>
 
         <b-field>
           <p class="control">
             <button 
-              type="submit" 
-              class="button is-primary is-pulled-right">
+              @click="sendInvoice" 
+              class="button is-primary is-pulled-right"
+              :class="{ 'is-loading': isLoading }">
               Enviar
             </button>
           </p>
         </b-field>
+
+        <b-field 
+          v-if="!!responseJSON"
+          label="Resposta">
+          <b-field label="Dados da NF:">
+            <b-input 
+              type="textarea"
+              v-model="responseJSON" 
+              readonly/>
+          </b-field>
+        </b-field>
       </form>
+
     </template>
-    
+
   </BcPageBase>
 </template>
 
 <script>
-// @ is an alias to /src
+import axios from 'axios';
+
+import BcField from '@/components/BcField.vue'
 import BcPageBase from '@/components/BcPageBase.vue'
 
 export default {
-  name: 'PageRegisterInvoice',
+  name: 'PageRegisterTransportUnit',
+  data() {
+    return {
+      isLoading: false,
+      invoiceJSON: null,
+      responseJSON: null,
+    };
+  },
+  methods: {
+    sendInvoice() {
+      this.isLoading = true;
+
+      axios.post('http://129.213.89.112:3000/api/AddNfe', JSON.parse(this.invoiceJSON))
+        .then(res => {
+          if (res.status === 200) {
+            console.log(res);
+
+            this.$toast.open({
+              message: 'Pacote cadastrado com sucesso!',
+              type: 'is-success',
+              position: 'is-bottom',
+            });
+
+            this.responseJSON = JSON.stringify(res.data);
+            this.isLoading = false;
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+
+          this.$toast.open({
+            message: 'Não foi possivel cadastrar o seu pacote',
+            type: 'is-danger',
+            position: 'is-bottom',
+          });
+
+          this.isLoading = false;
+        });
+    },
+  },
   components: {
-    BcPageBase
-  }
-}
+    BcField,
+    BcPageBase,
+  },
+};
 </script>
--- src/views/PageRegisterTransportUnit.vue
+++ src/views/PageRegisterTransportUnit.vue
@@ -6,34 +6,101 @@
     <template slot="content">
       <form class="form">
         <b-field label="Dados da Entrega:">
-          <b-input type="textarea"></b-input>
+          <b-input 
+            type="textarea"
+            v-model="transportUnitJSON" />
         </b-field>
 
         <b-field>
           <p class="control">
             <button 
-              type="submit" 
-              class="button is-primary is-pulled-right">
+              @click="sendTransportUnit" 
+              class="button is-primary is-pulled-right"
+              :class="{ 'is-loading': isLoading }">
               Enviar
             </button>
           </p>
         </b-field>
+
+        <b-field 
+          v-if="!!responseJSON"
+          label="Resposta">
+          <b-field label="Dados da Entrega:">
+            <b-input 
+              type="textarea"
+              v-model="responseJSON" 
+              readonly/>
+          </b-field>
+        </b-field>
       </form>
+
     </template>
 
   </BcPageBase>
 </template>
 
 <script>
-// @ is an alias to /src
+import axios from 'axios';
+
 import BcField from '@/components/BcField.vue'
 import BcPageBase from '@/components/BcPageBase.vue'
 
 export default {
   name: 'PageRegisterTransportUnit',
+  data() {
+    return {
+      isLoading: false,
+      transportUnitJSON: null,
+      responseJSON: null,
+    };
+  },
+  methods: {
+    sendTransportUnit() {
+      this.isLoading = true;
+
+      axios.post('http://129.213.89.112:3000/api/CreateTransportUnit', JSON.parse(this.transportUnitJSON))
+        .then(res => {
+          if (res.status === 200) {
+            console.log(res);
+
+            this.$toast.open({
+              message: 'Pacote cadastrado com sucesso!',
+              type: 'is-success',
+              position: 'is-bottom',
+            });
+
+            const {
+              transport_unit,
+              nfe_xml_base64,
+              nfe_key,
+              id,
+            } = res.data;
+
+            this.responseJSON = JSON.stringify({
+              transport_unit: `resource:org.logistic.network.TransportUnit#${id}`,
+              nfe_xml_base64,
+              nfe_key,
+            });
+
+            this.isLoading = false;
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+
+          this.$toast.open({
+            message: 'Não foi possivel cadastrar o seu pacote',
+            type: 'is-danger',
+            position: 'is-bottom',
+          });
+
+          this.isLoading = false;
+        });
+    },
+  },
   components: {
     BcField,
     BcPageBase,
-  }
-}
+  },
+};
 </script>
